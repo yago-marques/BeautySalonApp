d78e76c3405e5bfb32b07a76b88d9d0514fe0528
Milena Alcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-11-07 10:36:50 -0300

    init project and remove main storyboard

---
+++ SalonGo/SalonGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalonGo
+//
+//  Created by Milena Lima de Alcântara on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
